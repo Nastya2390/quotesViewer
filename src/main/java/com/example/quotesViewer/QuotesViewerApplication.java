cac919cb8987c1b75a6eee82f3c1a9011bbeae9b
kolosova_aa <kolosova_aa@interrao.ru>
2023-02-09 21:02:58 +0300

    init

---
+++ src/main/java/com/example/quotesViewer/QuotesViewerApplication.java
@@ -0,0 +1,13 @@
+package com.example.quotesViewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotesViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotesViewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/quotesViewer/QuotesViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quotesViewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotesViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
